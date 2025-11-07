55a01a22d989e2457488a4f4780e9a10de053b28
Zaheer <zaheer.aws@gmail.com>
2025-11-07 21:26:43 +0500

    init: expo typescript skeleton

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AppNavigation from './src/navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <AppNavigation />
+    </SafeAreaProvider>
   );
 }
 
